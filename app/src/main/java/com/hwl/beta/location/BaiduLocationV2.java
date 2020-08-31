a963b314a9b7a782dc5e17c3bddd5e461b98c088
v-yali3 <v-yali3@SADT-YALI3>
2020-08-31 15:56:02 +0800

    Updated cooridnate type is WGS84 for baidu location

--- app/src/main/java/com/hwl/beta/location/BaiduLocationV2.java
+++ app/src/main/java/com/hwl/beta/location/BaiduLocationV2.java
@@ -101,7 +101,8 @@ public class BaiduLocationV2 {
             mOption = new LocationClientOption();
             mOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);
             //可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
-            //mOption.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
+            //https://lbsyun.baidu.com/index.php?title=androidsdk/guide/coordtrans
+            mOption.setCoorType("WGS84");//可选，默认gcj02，设置返回的定位结果坐标系，如果配合百度地图使用，建议设置为bd09ll;
             //mOption.setScanSpan(3000);//可选，默认0，即仅定位一次，设置发起连续定位请求的间隔需要大于等于1000ms才是有效的
             mOption.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
             mOption.setIsNeedLocationDescribe(true);//可选，设置是否需要地址描述
@@ -114,8 +115,8 @@ public class BaiduLocationV2 {
             mOption.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation
             // .getPoiList里得到
             mOption.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
-            mOption.setOpenGps(true);//可选，默认false，设置是否开启Gps定位
-            mOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
+            //mOption.setOpenGps(true);//可选，默认false，设置是否开启Gps定位
+            //mOption.setIsNeedAltitude(false);//可选，默认false，设置定位时是否需要海拔信息，默认不需要，除基础定位版本都可用
             //设置打开自动回调位置模式，该开关打开后，期间只要定位SDK检测到位置变化就会主动回调给开发者，该模式下开发者无需再关心定位间隔是多少，定位SDK
             // 本身发现位置变化就会及时回调给开发者
             //mOption.setOpenAutoNotifyMode();
